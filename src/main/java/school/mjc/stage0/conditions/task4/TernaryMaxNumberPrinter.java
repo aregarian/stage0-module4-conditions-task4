94cfe190d72fc0ea386f28abb5a0d862bbbba5cb
Fanny Dorthey <fannydor@ev-k6.kzn.21-school.ru>
2023-10-19 06:29:06 +0300

    ouuuu my

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int big = first > second ? first : second;
+        System.out.println(big > third ? big : third);
     }
 }
